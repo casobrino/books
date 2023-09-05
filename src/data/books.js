f284256b2c5d1c06cc533cf55c9b0d954e02e6a6
Cesar Sobrino <casobrino_0@hotmail.com>
2023-09-05 08:36:27 -0600

    first commit

---
+++ src/data/books.js
@@ -0,0 +1,45 @@
+export const books=[
+    {
+        id: 'the-pragmatic-programmer',
+        title: 'The Progamatic Programer',
+        img: 'https://aprenderbigdata.com/wp-content/uploads/51cUVaBWZzL._SX380_BO1204203200_-mejores-libros-de-programacion.jpg',
+        desc:"Los autores de este libro se centran en dar toda una serie de consejos para convertirte en mejor programador de software y obtener mejores resultados.",
+        author: "Andrew Hunt y Dave Thomas"
+    },
+    {
+        id: 'the-art-of-computer-programming-fundamental-algorithms',
+        title: 'The Art of Computer Programming: Fundamental Algorithms',
+        img: 'https://aprenderbigdata.com/wp-content/uploads/41eCbcQARTL._SX342_BO1204203200_-mejores-libros-de-programacion.jpg.webp',
+        desc: "Este libro es un clásico del desarrollo de software. Aporta una visión fundamental y en profundidad de los algoritmos y estructuras de datos. Es complicado de leer y tiene bastantes explicaciones matemáticas con detalle, aun así, merece la pena por toda la información que concentra.",
+        author: "Donald E. Knuth"
+    },
+    {
+        id: 'cracking-the-coding-interview',
+        title: 'Cracking the Coding Interview',
+        img: 'https://aprenderbigdata.com/wp-content/uploads/51l5XzLln2BL._SX348_BO1204203200_-mejores-libros-de-programacion.jpg.webp',
+        desc: "El libro está centrado en el aspecto práctico de algoritmos y estructuras de datos. Además, como el título indica, incluye multitud de problemas con varios niveles de dificultad para aplicar lo explicado anteriormente y llegar a las soluciones",
+        author: "Gayle Laakmann McDowell"
+    },
+    {
+        id: 'clean-code-a-handbook-of-agile-software-craftsmanship',
+        title: 'Clean code: A Handbook of Agile Software Craftsmanship',
+        img: 'https://aprenderbigdata.com/wp-content/uploads/41K-wtoYjiL._SX387_BO1204203200_-mejores-libros-de-programacion.jpg.webp',
+        desc: "Incluye ejemplos y ejercicios para poner en práctica los consejos y patrones que se explican. Es uno de estos libros que se deberían leer periódicamente para refrescar todos los conceptos.",
+        author:"Robert C. Martin"
+    },
+    {
+        id: 'programming-in-scala',
+        title: 'The Progamatic Programer',
+        img: 'https://aprenderbigdata.com/wp-content/uploads/415cpVSxVmL._SX377_BO1204203200_-mejores-libros-de-programacion.jpg.webp',
+        desc: "Con este libro aprenderás a programar en Scala, muy usado en proyectos big data. El autor es el diseñador de este lenguaje de programación, que te enseñará paso a paso las ideas detrás de Scala para poder convertirte en experto. El libro cubre todo el lenguaje e importantes librerías.",
+        author:"Bert Bates, Kathy Sierra, Eric Freeman, Elisabeth Robson"
+    },
+    {
+        id: 'head-first-design-patterns',
+        title: 'Head First Design Patterns',
+        img: 'https://aprenderbigdata.com/wp-content/uploads/61ZG-hATOeL._SX430_BO1204203200_-mejores-libros-de-programacion.jpg.webp',
+        desc: "Edición actualizada de Java para ayudar a los programadores a mantenerse al día y aprovechar las nuevas versiones. También es un buen punto de partida para nuevos desarrolladores, que encontrarán numerosos ejemplos para aprovechar las APIS y mejores prácticas del lenguaje y su ecosistema.",
+        author:'Martin Odersky'
+    },
+
+]
